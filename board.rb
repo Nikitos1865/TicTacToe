3cc4be38f3260a04e7b9dc0a484946941bd3238e
Nikitos1865 <na2789@columbia.edu>
2024-03-06 17:24:15 -0500

    added a functionality to print out an example of the board, where the player can see where to place their X or O, and also added a player class that has a name and thecharacter that they play, whether that be an X or an O

--- board.rb
+++ board.rb
@@ -2,22 +2,22 @@ class Board
     attr_accessor :board
     def initialize
         self.board = {
-            0 => "X", 
-            1 => "O", 
-            2 => "O",
-            3 => "X", 
+            1 => " ", 
+            2 => " ", 
+            3 => " ",
             4 => " ", 
-            5 => " ",
-            6 => " ", 
+            5 => " ", 
+            6 => " ",
             7 => " ", 
-            8 => " "
+            8 => " ", 
+            9 => " "
     }
     end 
 
     def to_s
-        s1 = String.new("       #{self.board[0]} | #{self.board[1]} | #{self.board[2]}"+"\n"+"      -----------")
-        s2 = String.new("       #{self.board[3]} | #{self.board[4]} | #{self.board[5]}"+"\n"+"      -----------")
-        s3 = String.new("       #{self.board[6]} | #{self.board[7]} | #{self.board[8]}"+"\n")
+        s1 = String.new("       #{self.board[1]} | #{self.board[2]} | #{self.board[3]}"+"\n"+"      -----------")
+        s2 = String.new("       #{self.board[4]} | #{self.board[5]} | #{self.board[6]}"+"\n"+"      -----------")
+        s3 = String.new("       #{self.board[7]} | #{self.board[8]} | #{self.board[9]}"+"\n")
         s1+"\n"+s2+"\n"+s3
     end 
 
@@ -28,16 +28,17 @@ class Board
     def clear_board
         self.board.each_with_index {|val, key| board[key] = " "}
     end
+
+    def example_board 
+        s1 = String.new("       #{self.board.keys[0]} | #{self.board.keys[1]} | #{self.board.keys[2]}"+"\n"+"      -----------")
+        s2 = String.new("       #{self.board.keys[3]} | #{self.board.keys[4]} | #{self.board.keys[5]}"+"\n"+"      -----------")
+        s3 = String.new("       #{self.board.keys[6]} | #{self.board.keys[7]} | #{self.board.keys[8]}"+"\n")
+        s1+"\n"+s2+"\n"+s3
+    end 
 end 
 
-bord = Board.new(); 
-puts bord.board;
-puts bord 
+bord = Board.new()
 
+puts bord.example_board
 
-bord.clear_board
-
-bord.mark_board("X", 5)
-
-puts bord 
 
---
+++ player.rb
@@ -0,0 +1,10 @@
+class Player 
+  attr_accessor :name, :play
+
+  def initialize(nym, char)
+    self.name = nym; 
+    self.play = char; 
+  end 
+end 
+
+ 
